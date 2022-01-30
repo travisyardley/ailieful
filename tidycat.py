881a3d5c1959615cfdcb8b000973c1e2536cd52c
Travis Yardley <86284508+travisyardley@users.noreply.github.com>
2022-01-29 18:14:17 -0800

    Add files via upload

--- tidycat.py
+++ tidycat.py
@@ -14,14 +14,15 @@ eda_db = sqlite.connect('eda-data_db.sqlite')
 eda_sql = eda_db.cursor()
 
 # Checks the database to ensure the correct tables exists, and generates them
-# if they are missing.
+# if they are missing. Also populates my fake boolean values because SQL doesn't
+# support boolean logic for some reason?
 eda_sql.execute('CREATE TABLE IF NOT EXISTS companies (id INTEGER PRIMARY KEY, company TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS brands (id INTEGER PRIMARY KEY, brand TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS flavours (id INTEGER PRIMARY KEY, flavour TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS companies (id INTEGER PRIMARY KEY, company TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS analysis_type(id BIT PRIMARY KEY, type TEXT UNIQUE)')
-eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES ('ga')')
-eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES ('ta')')
+eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES (?)', ('ga'))
+eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES (?)', ('ta'))
 eda_sql.execute('CREATE TABLE IF NOT EXISTS valcalc_type(id BIT PRIMARY KEY, type TEXT UNIQUE)')
-eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES ('dmb')')
-eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES ('wmb')')
+eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES (?)', ('dmb'))
+eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES (?)', ('wmb'))
