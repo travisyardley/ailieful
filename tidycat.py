07a463c3fec916317b0528f3b7a2f88682567c5e
Travis Yardley <86284508+travisyardley@users.noreply.github.com>
2022-01-29 19:04:18 -0800

    Add files via upload
    Introduced read only to the raw-data_db.sqlite connection.

--- tidycat.py
+++ tidycat.py
@@ -8,8 +8,11 @@
 
 # Python Library imports :
 import sqlite3 as sqlite
+import re
 
-# Establishes connection to SQL server and an associated cursor method.
+# Opens a connection for each database to be used, and limits the raw data to be read only.
+raw_db = sqlite.connect('file:raw-data_db.sqlite?mode=ro', uri=True)
+raw_sql = raw_db.cursor()
 eda_db = sqlite.connect('eda-data_db.sqlite')
 eda_sql = eda_db.cursor()
 
@@ -20,9 +23,17 @@ eda_sql.execute('CREATE TABLE IF NOT EXISTS companies (id INTEGER PRIMARY KEY, c
 eda_sql.execute('CREATE TABLE IF NOT EXISTS brands (id INTEGER PRIMARY KEY, brand TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS flavours (id INTEGER PRIMARY KEY, flavour TEXT UNIQUE)')
 eda_sql.execute('CREATE TABLE IF NOT EXISTS companies (id INTEGER PRIMARY KEY, company TEXT UNIQUE)')
+# Creating a table that will fake boolean logic for me.
 eda_sql.execute('CREATE TABLE IF NOT EXISTS analysis_type(id BIT PRIMARY KEY, type TEXT UNIQUE)')
 eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES (?)', ('ga'))
 eda_sql.execute('INSERT OR IGNORE INTO analysis_type (type) VALUES (?)', ('ta'))
+# As above, faking that boolean logic.
 eda_sql.execute('CREATE TABLE IF NOT EXISTS valcalc_type(id BIT PRIMARY KEY, type TEXT UNIQUE)')
 eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES (?)', ('dmb'))
 eda_sql.execute('INSERT OR IGNORE INTO valcalc_type (type) VALUES (?)', ('wmb'))
+
+# Building tidycat specific functions.
+
+
+# Pulling in HTML code from raw-data_db.sqlite for sorting.
+ 
